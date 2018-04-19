94e9649ee7ba7e614a57f6c3a559f98dacf58910
adamyyy <adamyablonka@gmail.com>
2018-04-20 02:17:09 +0300

    Firebase and git up and running

--- app/src/main/java/com/example/admin/liftapp/MainActivity.java
+++ app/src/main/java/com/example/admin/liftapp/MainActivity.java
@@ -3,11 +3,18 @@ package com.example.admin.liftapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+//Write something to our DB
+        myRef.setValue("Hello, World!");
         setContentView(R.layout.activity_main);
     }
 }
